75f0784a725d9413a35b15dca32c0dd4a8b4cfb2
avivDan <42783558+avivdan@users.noreply.github.com>
2020-11-14 19:17:17 +0200

    Update power.c

--- HW1/power.c
+++ HW1/power.c
@@ -3,18 +3,33 @@
 double exponent(int x){
 	int a;
 	double b = 1;
-	for( a = 0; a < x; a = a + 1 ){
-    		b=b*exp;
-    	}
-    	return b;
+	if(x<0){
+		for(a=0; a< -x; a = a + 1){
+			b = b/exp;
+		}
+		return b;
+	}else{
+		for( a = 0; a < x; a = a + 1 ){
+	    		b=b*exp;
+	    	}
+	    	return b;
+	}
 }
 
 double power(double x, int y){
 	int a;
 	double b = 1;
-	for( a = 0; a < y; a = a + 1 ){
-    		b=b*x;
-    	}
-    	return b;
+	if(y<0){
+		for( a = 0; a < -y; a = a + 1 ){
+	    		b=b/x;
+	    	}
+	    	return b;
+	}else{
+		for( a = 0; a < y; a = a + 1 ){
+	    		b=b*x;
+	    	}
+	    	return b;
+	}
+	
 }
 
